cd0df40baa4a62110a9cbfb982a924bf2301eb2e
brady gaster <bradyg@microsoft.com>
2019-12-13 14:20:16 -0800

    smooth scrolling

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,4 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your Javascript code.
+﻿window.scrollIntoView = () => {
+    console.log('hey');
+    document.getElementById('loadMorePostsButton').scrollIntoView({behavior: 'smooth'});
+};
